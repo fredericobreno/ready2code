a16f72ea48d750fad2b4101b437e6e6856f1bf25
Frederico Breno <fredrochaceara@gmail.com>
2024-01-30 01:11:50 -0300

    eslint configuration added, inquirer and ora added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log(1)
+import inquirer from 'inquirer'
+import { Eslint } from './steps'
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'packageManager',
+      message: 'Which package manager do you want to use?',
+      choices: ['npm', 'yarn', 'pnpm'],
+    },
+    {
+      type: 'list',
+      name: 'eslintStack',
+      message: 'Which stack do you want to use?',
+      choices: [
+        { name: 'React (with Next.js)', value: 'next' },
+        { name: 'React (without Next.js)', value: 'react' },
+        { name: 'Node.js', value: 'node' },
+      ],
+    },
+  ])
+  .then(async (answers) => {
+    const { packageManager, eslintStack } = answers
+
+    await Eslint.install({ packageManager })
+    await Eslint.configure({ eslintStack })
+  })
---
+++ src/steps.ts
@@ -0,0 +1,57 @@
+import ora from 'ora'
+import { exec } from 'child_process'
+import util from 'util'
+import fs from 'fs'
+
+const execPromise = util.promisify(exec)
+
+export const Eslint = {
+  install: async ({ packageManager }: { packageManager: string }) => {
+    const spinner = ora('installing eslint...').start()
+    const installCmd = packageManager === 'yarn' ? 'add' : 'install'
+
+    try {
+      const { stderr } = await execPromise(
+        `${packageManager} ${installCmd} -D eslint @rocketseat/eslint-config`,
+      )
+
+      if (stderr) {
+        spinner.fail('error trying to install eslint')
+        console.error(stderr)
+        return
+      }
+
+      spinner.succeed('eslint installed successfully!')
+    } catch (err) {
+      spinner.fail('error trying to install eslint')
+      console.error(err)
+    }
+  },
+  configure: async ({ eslintStack }: { eslintStack: string }) => {
+    const spinner = ora('configuring eslint...').start()
+
+    try {
+      const eslintFile = fs.readFileSync('.eslintrc.json', 'utf-8')
+      const eslintConfig = JSON.parse(eslintFile)
+
+      if (eslintConfig.extends) {
+        eslintConfig.extends.push(`@rocketseat/eslint-config/${eslintStack}`)
+      } else {
+        eslintConfig.extends = [`@rocketseat/eslint-config/${eslintStack}`]
+      }
+
+      fs.writeFileSync('.eslintrc.json', JSON.stringify(eslintConfig, null, 2))
+      spinner.succeed('eslint configured successfully!')
+    } catch (err) {
+      if ((err as { code: string }).code === 'ENOENT') {
+        fs.writeFileSync('.eslintrc.json', '{ "extends": [] }')
+        Eslint.configure({ eslintStack })
+        spinner.stop()
+        return
+      }
+
+      spinner.fail('error trying to configure eslint')
+      console.error(err)
+    }
+  },
+}
