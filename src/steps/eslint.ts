35bc73b5b93741a7e26f35dcab03f80fbe34ee07
Frederico Rocha <fredericorocha@penzack.com>
2024-02-09 13:31:45 -0300

    eslint config step fixed

--- src/steps/eslint.ts
+++ src/steps/eslint.ts
@@ -16,31 +16,10 @@ export const Eslint: Step = {
   },
   configure: async (options) => {
     const eslintStack = options?.eslintStack || 'node'
-    const eslintFile = fs.readFileSync('.eslintrc.json', 'utf-8')
-    const eslintConfig = JSON.parse(eslintFile)
-
-    try {
-      if (eslintConfig.extends) {
-        if (Array.isArray(eslintConfig.extends)) {
-          eslintConfig.extends.push(`@rocketseat/eslint-config/${eslintStack}`)
-        } else {
-          eslintConfig.extends = [
-            eslintConfig.extends,
-            `@rocketseat/eslint-config/${eslintStack}`,
-          ]
-        }
-      } else {
-        eslintConfig.extends = [`@rocketseat/eslint-config/${eslintStack}`]
-      }
-
-      eslintConfig.extends = [...new Set([...eslintConfig.extends])]
-
-      fs.writeFileSync('.eslintrc.json', JSON.stringify(eslintConfig, null, 2))
-    } catch (err) {
-      if ((err as { code: string }).code === 'ENOENT') {
-        fs.writeFileSync('.eslintrc.json', '{ "extends": [] }')
-        Eslint.configure({ eslintStack })
-      }
+    const eslintConfig: { extends: string[] } = {
+      extends: [`@rocketseat/eslint-config/${eslintStack}`],
     }
+
+    fs.writeFileSync('.eslintrc.json', JSON.stringify(eslintConfig, null, 2))
   },
 }
